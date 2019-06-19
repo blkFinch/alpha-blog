92699b90bdea564173f2c3141d67fcc5d8455e50
finch <blkfinch@gmail.com>
2019-06-19 16:04:00 -0600

    fixed style

--- Gemfile
+++ Gemfile
@@ -27,7 +27,7 @@ gem 'jbuilder', '~> 2.5'
 # gem 'bcrypt', '~> 3.1.7'
 
 gem 'haml-rails'
-gem 'bootstrap-sass', '~> 3.4.1'
+gem 'bootstrap', '~>4.0.0'
 gem 'jquery-rails'
 
 # Use ActiveStorage variant
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -27,8 +27,7 @@ class ArticlesController < ApplicationController
     @article = Article.new(article_params)
 
     if @article.save
-      flash[:notice] =  'Article was successfully created.'
-      redirect_to @article
+      redirect_to @article, notice: 'Article was successfully created.'
     else
       render 'new'
     end
