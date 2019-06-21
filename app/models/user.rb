2e6d04925cb728ba254c3b88c32d0e45fcde857c
finch <blkfinch@gmail.com>
2019-06-21 12:18:35 -0600

    added validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ApplicationRecord
+  require 'uri' #for email validation
+
+  validates :username, presence: true,
+            uniqueness: {case_sensitive: false},
+            length: {minimum: 3, maximum: 30}
+
+  validates :email, presence: true,
+            uniqueness: {case_sensitive: false},
+            length: {maximum: 100 }, format: { with: URI::MailTo::EMAIL_REGEXP, message: "only allows valid emails" }
+
 end
