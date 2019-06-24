bd9162221a99231f621b8ead90509c4388a58059
finch <blkfinch@gmail.com>
2019-06-24 13:35:42 -0600

    added user show

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   resources :articles
   get 'about', to:'pages#about'
+  get 'signup', to:'users#new'
+
+  resources :users, except: [:new]
 
   root 'pages#home'
 
